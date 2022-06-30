d7c93ef6e08c189a0058fdcbcfbef88bc0ccb37f
Garci4 <juliangarcia.edi@gmail.com>
2022-06-29 22:25:20 -0300

    ahora hay regulaciones

--- app/src/main/java/com/example/pescarecreativa/fragments/FragmentRegulaciones.kt
+++ app/src/main/java/com/example/pescarecreativa/fragments/FragmentRegulaciones.kt
@@ -1,21 +1,35 @@
 package com.example.pescarecreativa.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.pescarecreativa.R
+import com.example.pescarecreativa.adapter.ReporteAdapter
+import com.example.pescarecreativa.databinding.ActivityHomeBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.io.PrintStream
+
 
 class FragmentRegulaciones : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_regulaciones, container, false)
-    }
+        var rootView = inflater.inflate(R.layout.fragment_regulaciones, container, false)
 
+        return rootView
+    }
 
 }
